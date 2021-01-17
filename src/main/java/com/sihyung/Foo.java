62b51367177079457db1cb527071413e62e7df90
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-01-17 18:57:58 +0900

    Function의 Compose 활용

--- src/main/java/com/sihyung/Foo.java
+++ src/main/java/com/sihyung/Foo.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 public class Foo {
     public static void main(String[] args) {
         Function<Integer, Integer> plus10 = (i) -> i + 10;
-        System.out.println(plus10.apply(1));
+        Function<Integer, Integer> multiply2 = (i) -> i * 2;
+        // compose 안에 Function 먼저 실행 후 결과값으로 해당 Function 실행
+        System.out.println(plus10.compose(multiply2).apply(10));
     }
 }
