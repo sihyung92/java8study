92fded72e5c0e6d91ec2298b3541f6c3bae3c63e
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-01-17 18:54:15 +0900

    Function 활용

---
+++ src/main/java/com/sihyung/Foo.java
@@ -0,0 +1,8 @@
+package com.sihyung;
+
+public class Foo {
+    public static void main(String[] args) {
+        Plus10 plus10 = new Plus10();
+        System.out.println(plus10.apply(1));
+    }
+}
