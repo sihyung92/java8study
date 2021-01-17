61df4386d4fb98f9ffe408ed4aa4d148f04d65bb
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-01-17 18:55:26 +0900

    Function 활용, lambda로 선언

--- src/main/java/com/sihyung/Foo.java
+++ src/main/java/com/sihyung/Foo.java
@@ -1,8 +1,10 @@
 package com.sihyung;
 
+import java.util.function.Function;
+
 public class Foo {
     public static void main(String[] args) {
-        Plus10 plus10 = new Plus10();
+        Function<Integer, Integer> plus10 = (i) -> i + 10;
         System.out.println(plus10.apply(1));
     }
 }
