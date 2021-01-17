25fb2c13d05b668730bc57e0abb207d3ff3a929c
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-01-17 19:08:37 +0900

    UnaryOperator, BinaryOperator 학습

--- src/main/java/com/sihyung/Foo.java
+++ src/main/java/com/sihyung/Foo.java
@@ -1,25 +1,18 @@
 package com.sihyung;
 
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class Foo {
     public static void main(String[] args) {
-        //BiFunction은 인자를 2개받음
+        //Function 인데 입출력 타입이 같을 때 사용
+        UnaryOperator<Integer> unaryOperator = (i) -> i + 10;
 
-        //Consumer 예제 i -> x
-        Consumer<Integer> printT = (i) -> System.out.println(i);
-
-        //Supplier 예제 x -> i
-        Supplier<Integer> get10 = () -> 10;
-
-        //Predicate 예제 x -> T/F
-        Predicate<String> startsWithSihyung = (s) -> s.startsWith("Sihyung");
-        // And, or, negate 등 조합 가능
-        //startsWithSihyung.and(predicateB);
-        //startsWithSihyung.or(predicateB);
-
+        //BiFunction 인데 <T,T,T> 인 상황에 사용
+        BinaryOperator<Integer> binaryOperator = (i1, i2) -> i1 + i2 + 10;
     }
 }
