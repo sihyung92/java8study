9fcaa816c2b2fd552a8ee75dcad7247d8525455f
Sung Si Hyung <fjzjqhdl@gmail.com>
2021-01-20 17:58:13 +0900

    임의의 객체의 메소드 참조

--- src/main/java/com/sihyung/Foo.java
+++ src/main/java/com/sihyung/Foo.java
@@ -1,5 +1,6 @@
 package com.sihyung;
 
+import java.util.Arrays;
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -10,8 +11,8 @@ import java.util.function.UnaryOperator;
 public class Foo {
     public static void main(String[] args) {
         // 인자가 있는 생성자 참조
-        Function<String, Greeting> sihyungGreeting = Greeting::new;
-        Greeting greeting = sihyungGreeting.apply("sihyung");
-        System.out.println(greeting.getName());
+        String[] names = {"sihyung", "sujin", "love"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+        Arrays.sort(names, ((o1, o2) -> 0));
     }
 }
